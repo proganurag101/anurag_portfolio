84024c26594d566cbd8f7b6f8af7cf0c209ca7e7
proganurag101 <anurag94530@gmail.com>
2021-09-01 21:06:21 +0530

    2nd commit

--- mail.php
+++ mail.php
@@ -2,17 +2,13 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
-$subject= $_POST['subject'];
 $message= $_POST['message'];
-
-$to = "anurag94530@gmail.com";
-$subject = "Mail From you portfolio website";
+$to = "anurag94530@mail.com";
+$subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+"CC: pandeyjack101@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
-//redirect
-header("Location:thankyou.html");
 ?>
